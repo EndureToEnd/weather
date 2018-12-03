e596e7600ad5fee6bc0202e85694d634080a1174
ZengJun <1924131534@qq.com>
2018-12-03 15:09:03 +0800

    加入创建的数据库和表的各项配置。

---
+++ app/src/main/java/com/example/tobethebest/lab6exer001/db/Provice.java
@@ -0,0 +1,33 @@
+package com.example.tobethebest.lab6exer001.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Provice extends DataSupport {
+    private int id;
+    private  String provinceName;
+    private int proviceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProviceCode() {
+        return proviceCode;
+    }
+
+    public void setProviceCode(int proviceCode) {
+        this.proviceCode = proviceCode;
+    }
+}
